aaee2118ca040ccad5104f2ec2c68e3b83b52f40
Artemy Gibson <artemygibson@gmail.com>
2023-09-08 09:53:27 -0700

    Break up original method into two

--- app/services/avatar_service.rb
+++ app/services/avatar_service.rb
@@ -1,5 +1,9 @@
 class AvatarService
   def get_characters(query)
-    Faraday.get("https://last-airbender-api.fly.dev/api/v1/characters?perPage=10000&affiliation=#{query}")
+    connection.get("characters?perPage=10000&affiliation=#{query}")
+  end
+  
+  def connection 
+    Faraday.new("https://last-airbender-api.fly.dev/api/v1/")
   end
 end
