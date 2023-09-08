6fa6b94822ea58c4f2d940b7b0c3551557698217
Artemy Gibson <artemygibson@gmail.com>
2023-09-08 09:10:25 -0700

    Add query param to increase limit of characters returned

--- app/services/avatar_service.rb
+++ app/services/avatar_service.rb
@@ -1,5 +1,5 @@
 class AvatarService
   def get_characters(query)
-    Faraday.get("https://last-airbender-api.fly.dev/api/v1/characters?affiliation=#{query}")
+    Faraday.get("https://last-airbender-api.fly.dev/api/v1/characters?perPage=10000&affiliation=#{query}")
   end
 end
