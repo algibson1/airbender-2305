75885f6a589d3b71f21f6a304cc44d6a9e99ec39
Artemy Gibson <artemygibson@gmail.com>
2023-09-08 09:33:09 -0700

    Remove white space in expected return values

--- spec/poros/character_spec.rb
+++ spec/poros/character_spec.rb
@@ -14,8 +14,8 @@ RSpec.describe Character do
     expect(azula.id).to eq("5cf5679a915ecad153ab68da")
     expect(azula.name).to eq("Azula")
     expect(azula.photo).to eq("https://vignette.wikia.nocookie.net/avatar/images/1/12/Azula.png/revision/latest?cb=20140905084941")
-    expect(azula.allies).to eq("Ozai, Zuko ")
-    expect(azula.enemies).to eq("Iroh, Zuko, Kuei, Long Feng, Mai, Ty Lee, Ursa ")
-    expect(azula.affiliations).to eq(" Azula's team (formerly) Dai Li (formerly) Fire Nation Fire Nation Royal Family Fire Warriors Royal Fire Academy for Girls (formerly)")
+    expect(azula.allies).to eq("Ozai, Zuko")
+    expect(azula.enemies).to eq("Iroh, Zuko, Kuei, Long Feng, Mai, Ty Lee, Ursa")
+    expect(azula.affiliations).to eq("Azula's team (formerly) Dai Li (formerly) Fire Nation Fire Nation Royal Family Fire Warriors Royal Fire Academy for Girls (formerly)")
   end
 end
