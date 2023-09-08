2c7eb129b4f0aece2612c85c97cfb4f05671b528
Artemy Gibson <artemygibson@gmail.com>
2023-09-08 09:09:15 -0700

    Add additional expects statements to test

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Search Index" do
   context "when searching for Fire Nation residents" do
-    it "shows all Fire Nation residents and their information" do
+    it "shows 25 Fire Nation residents and their information" do
       # When I visit "/"
       visit root_path
 
@@ -16,26 +16,27 @@ RSpec.describe "Search Index" do
       expect(current_path).to eq(search_path)
 
       # Then I should see the total number of people who live in the Fire Nation. (should be close to 100)
-      expect(page).to have_content("Total Fire Nation Members:")
+      expect(page).to have_content("Total Fire Nation Members: 97")
 
       # And I should see a list with the detailed information for the first 25 members of the Fire Nation.
-      ####Expect there to be 25 blocks of character info
+      expect(all(".character_info").count).to eq(25)
 
       # And for each of the members I should see:
-      # characters = Facade Call
-      #characters.each do |character|
-      # within(tag with character id)
-      # expect to find name of character
-      # expect a photo
-      #expect a list of allies OR None
-      # expect a list of enemies OR none
-      # expect affiliations
+      characters = SearchFacade.new.search("fire+nation")
+      characters.each do |character|
+        within("#character-#{character.id}") do
+          # - The name of the member (and their photo, if they have one)
+          expect(page).to have_content(character.name)
+          expect(page).to have_selector(img)
+          # - The list of allies or "None"
+          expect(page).to have_content("Allies: #{!character.allies.empty? ? character.allies : "None"}")
+          # - The list of enemies or "None"
+          expect(page).to have_content("Enemies: #{!character.enemies.empty? ? character.enemies : "None"}")
+          # - Any affiliations that the member has
+          expect(page).to have_content("Affiliations: #{character.affiliations.empty? ? character.affiliations : "None"}")
+        end
+      end
     end
   end
 end
 
-
-# - The name of the member (and their photo, if they have one)
-# - The list of allies or "None"
-# - The list of enemies or "None"
-# - Any affiliations that the member has
